05bc714186e08a258d9a9af932b783d1525a8223
Dilip R <diliprbtech24@rvu.edu.in>
2026-09-24 21:41:28 +0530

    Fix BrowserRouter import and add basename

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import <BrowserRouter basename="/ESM-CNC"> from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { AppProvider } from './context/AppContext'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/ESM-CNC">
       <AppProvider>
         <App />
       </AppProvider>
